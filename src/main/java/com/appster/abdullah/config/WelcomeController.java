17317c0f666a1e5ca66ea89db016c7e8da885264
Abdullah8006 <abeiy4u@gmail.com>
2018-01-28 14:33:53 +0300

    First commit

--- src/main/java/com/appster/abdullah/config/WelcomeController.java
+++ src/main/java/com/appster/abdullah/config/WelcomeController.java
@@ -1,7 +1,5 @@
 package com.appster.abdullah.config;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
@@ -9,92 +7,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.appster.abdullah.entity.Institute;
-import com.appster.abdullah.entity.InstituteClass;
-import com.appster.abdullah.entity.User;
-import com.appster.abdullah.repository.InstituteClassRepository;
-import com.appster.abdullah.repository.InstituteRepository;
-import com.appster.abdullah.repository.UserRepository;
-import com.appster.abdullah.service.UserService;
-import com.appster.abdullah.springIntegration.TestGateway;
-
 @Controller
 public class WelcomeController {
 
 	private static final Logger LOG = Logger.getLogger(WelcomeController.class);
-	private TestGateway gateway;
-	private UserService userService;
-	@Autowired
-	private InstituteClassRepository instituteClassRepository;
-	@Autowired
-	private UserRepository userRepository;
-	@Autowired
-	private InstituteRepository instituteRepository;
 
 	@Autowired
-	public WelcomeController(TestGateway gateway, UserService userService) {
-		this.gateway = gateway;
-		this.userService = userService;
+	public WelcomeController() {
+		
 	}
 
 	@RequestMapping("/")
 	public String welcome(Map<String, Object> model) {
-		// gateway.updateUserStatistics(MessageBuilder.withPayload(getUsers()).build());
-
+	    LOG.info("Home page accessed.");
 		model.put("message", "User name : " + " null as of now!");
-		return "welcome";
-	}
-
-	@RequestMapping("/addInstituteClasses")
-	public String savaUser(Map<String, Object> model) {
-
-		Institute institute = instituteRepository.findOne((long) 1);
-
-		List<InstituteClass> classes = new ArrayList<>();
-
-		InstituteClass classOne = new InstituteClass();
-		classOne.setName("Class One");
-		classOne.setInstitute(institute);
-		classes.add(classOne);
-
-		InstituteClass classTwo = new InstituteClass();
-		classTwo.setName("Class Two");
-		classTwo.setInstitute(institute);
-		classes.add(classTwo);
-
-		instituteClassRepository.save(classes);
-
-		LOG.info("Persisting Customers and there associated addresses");
-		return "welcome";
-	}
-
-	@RequestMapping("/testCriteria")
-	public String testCriteria() {
-		LOG.info("Persisting Customers and there associated addresses");
-		userService.testCriteriaQuery();
-		return "welcome";
-	}
-
-	private List<User> getUsers() {
-		List<User> users = new ArrayList<>();
-
-		User userOne = new User();
-		userOne.setUsername("Aamir Arafat");
-
-		User userTwo = new User();
-		userTwo.setUsername("Arsalan Aslam");
-
-		users.add(userOne);
-		users.add(userTwo);
-
-		return users;
-	}
-
-	@RequestMapping("/user")
-	public String addUser() {
-		List<User> users = getUsers();
-		for (User user : users)
-			userService.addUser(user);
 		return "welcome";
 	}
 
